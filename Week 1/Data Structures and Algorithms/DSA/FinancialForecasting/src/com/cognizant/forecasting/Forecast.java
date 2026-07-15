a64380803b8458804afa8f02832065ed48015aa8
Joshitha A N <joshithaan.dev@gmail.com>
2026-07-15 21:46:32 +0530

    Add files via upload

---
+++ Week 1/Data Structures and Algorithms/DSA/FinancialForecasting/src/com/cognizant/forecasting/Forecast.java
@@ -0,0 +1,28 @@
+package com.cognizant.forecasting;
+
+public class Forecast {
+
+    // Recursive method to calculate future value
+    public static double predictValue(double currentValue, double growthRate, int years) {
+
+        if (years == 0) {
+            return currentValue;
+        }
+
+        return predictValue(currentValue * (1 + growthRate), growthRate, years - 1);
+    }
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10;   // 10%
+        int years = 5;
+
+        double futureValue = predictValue(currentValue, growthRate, years);
+
+        System.out.println("Current Value : " + currentValue);
+        System.out.println("Growth Rate   : " + (growthRate * 100) + "%");
+        System.out.println("Years         : " + years);
+        System.out.printf("Future Value  : %.2f", futureValue);
+    }
+}
