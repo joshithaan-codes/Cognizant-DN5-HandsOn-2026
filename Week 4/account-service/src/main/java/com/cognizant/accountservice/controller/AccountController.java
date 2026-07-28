7ae59d58288257cf98a5d2509c4e74def5b52a1b
Joshitha A N <joshithaan.dev@gmail.com>
2026-07-28 10:44:22 +0530

    Completed account-service Hands On

---
+++ Week 4/account-service/src/main/java/com/cognizant/accountservice/controller/AccountController.java
@@ -0,0 +1,19 @@
+package com.cognizant.accountservice.controller;
+
+import com.cognizant.accountservice.model.Account;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/accounts")
+    public Account getAccount() {
+
+        return new Account(
+                "123456789",
+                "Savings",
+                50000
+        );
+    }
+}
